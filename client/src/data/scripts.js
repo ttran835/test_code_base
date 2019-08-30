89f6ed122daecdc6316c322d6d3116a3f370466b
ttran835 <timothy.tran835@gmail.com>
2019-08-29 23:48:26 -0700

    Update JS code to render dynamic data information based on information from json data, this is testing purposes only, may not work if data is changed

--- client/src/data/scripts.js
+++ client/src/data/scripts.js
@@ -21,24 +21,90 @@ const FakeDataToRender = () => {
     },
     {
       header: 'Material',
-      type: [
-        {
-          Cashmere: '50%',
-          Wool: '46%',
-          Modal: '4%',
-        },
-      ],
+      type: {
+        Cashmere: '50%',
+        Wool: '46%',
+        Modal: '4%',
+      },
+
       description:
         'Aenean vel felis eu ipsum pharetra finibus. Proin accumsan eleifend dui, in convallis mauris varius sed. Nam luctus justo ut placerat efficitur. Morbi aliquet, lacus a dictum luctus, neque neque fermentum risus, rhoncus accumsan ex sapien ut nisi. Aenean sit amet mattis turpis. Nullam diam erat, sagittis sed laoreet sed, porta a nulla. Mauris luctus aliquet est, sit amet maximus dolor tincidunt vitae. Vestibulum porttitor maximus arcu, et bibendum urna semper id. Integer id sagittis urna. Curabitur nisl sapien, pharetra placerat quam a, tempus dignissim dui. Nunc fermentum dui nec ex blandit, vitae lacinia ante laoreet.',
     },
   ];
 };
 
-FakeDataToRender.prototype.getData = () => {
-  return this.storage;
+FakeDataToRender.prototype.storeData = () => {
+  const storeData = this.storage;
+  let newDataObj = {};
+
+  storeData.forEach((data, i, arr) => {
+    const description = data.description;
+    const materialType = data.type;
+    newDataObj[data.header] = { description, materialType };
+  });
+
+  return newDataObj;
 };
 
-const data = new FakeDataToRender();
-const fakeData = data.getData();
+FakeDataToRender.prototype.renderData = object => {
+  const fit = document.getElementById('fit');
+  const care = document.getElementById('care');
+  const materials = document.getElementById('materials');
+  const materialStats = document.getElementById('material-stats');
 
-console.log({ fakeData });
+  for (let keys in object) {
+    const label = document.createElement('LABEL');
+    const paragraph = document.createElement('P');
+    const description = object[keys].description;
+    const materialObj = object[keys].materialType;
+
+    switch (keys) {
+      case 'Fit Guide':
+        label.innerHTML = keys;
+        paragraph.innerHTML = description;
+        fit.appendChild(label);
+        fit.appendChild(paragraph);
+        break;
+
+      case 'Care':
+        label.innerHTML = keys;
+        paragraph.innerHTML = description;
+        care.appendChild(label);
+        care.appendChild(paragraph);
+        break;
+
+      case 'Material':
+        label.innerHTML = keys;
+        paragraph.innerHTML = description;
+        materials.appendChild(label);
+
+        for (let keys in materialObj) {
+          const singleMaterialDiv = document.createElement('DIV');
+          singleMaterialDiv.classList.add('material');
+          let materialP = document.createElement('P');
+          let span = document.createElement('SPAN');
+          materialP.innerHTML = materialObj[keys];
+          span.innerHTML = keys;
+
+          singleMaterialDiv.appendChild(span);
+          singleMaterialDiv.appendChild(materialP);
+          materialStats.appendChild(singleMaterialDiv);
+        }
+        materials.appendChild(paragraph);
+        break;
+
+      default:
+    }
+  }
+};
+
+const AnimationAndEventTriggers = () => {
+  this.storage = [];
+};
+
+AnimationAndEventTriggers.prototype.renderTabs = () => {};
+
+const newData = new FakeDataToRender();
+const newDataObj = newData.storeData();
+const createDivs = newData.renderData(newDataObj);
+console.log({ newDataObj });
